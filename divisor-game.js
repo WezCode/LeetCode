d54cc0a0d76d60f8a1836cdafbd09a404da9bdb6
User <wesley.rmit@gmail.com>
2020-08-09 21:02:00 +1000

    Solved smallest range i

---
+++ divisor-game.js
@@ -0,0 +1,14 @@
+
+
+var divisorGame = function (N) {
+    let alicesTurn = true;
+    let chalkboardNumber = N;
+
+    // Find highest factor
+    // If number is even, largest factor is 1/2
+    // If the number is divisible by 3, 
+    if (N)
+
+
+
+};
---
+++ smallest-range-i.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} A
+ * @param {number} K
+ * @return {number}
+ * https://leetcode.com/problems/smallest-range-i/submissions/
+ */
+var smallestRangeI = function (A, K) {
+    // Find the biggest number
+
+    var sorted = A.slice().sort(function (a, b) {
+        return a - b;
+    });
+
+    if (A.length != 1) {
+        var value;
+        let initialDiff = sorted[sorted.length - 1] - sorted[0];
+        if (initialDiff > K) {
+            if (K * 2 > initialDiff) {
+                return 0;
+            } else {
+                value = (sorted[sorted.length - 1] - K) - (sorted[0] + K);
+                console.log(value);
+                return value;
+            }
+        } else {
+            value = (sorted[sorted.length - 1] + K) - (sorted[0] + K);
+            console.log(value);
+            return 0;
+        }
+    } else {
+        return 0;
+    }
+
+};
